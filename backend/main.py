5a52eb6a166c3d25eb46eabd47f77dc4e4eb14af
Shrish <shrishmak986@gmail.com>
2025-08-25 06:28:16 +0530

    Fixed a bug on address

--- backend/main.py
+++ backend/main.py
@@ -310,7 +310,7 @@ async def http_exception_handler(request, exc):
 if __name__ == "__main__":
     uvicorn.run(
         "main:app",
-        host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", 8000)),
+        host="127.0.0.1",   
+        port=8000,          
         reload=True
     )
